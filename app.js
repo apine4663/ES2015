0065ff3f2b2ec990a0acc605b1484547d65e6251
Alex <apine4663@outlook.com>
2022-01-19 10:43:03 -0600

    letAndConstExercise

---
+++ app.js
@@ -0,0 +1,14 @@
+// var PI = 3.14;
+// PI = 42;
+const PI = 3.14;
+
+// What is the difference between var and let?
+    //var allows redeclaration while let does not
+
+// What is the difference between var and const?
+    //You can redeclare and reassign with var but not with const. const is also block-scoped
+// What is the difference between let and const?
+    //Let allows reassigning while const does not
+// What is hoisting?
+    //The method that a JS compiler uses to view code
+
