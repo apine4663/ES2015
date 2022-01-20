a33cd9d53c1851e0f52afcfdcbf538bffbc94bce
Alex <apine4663@outlook.com>
2022-01-20 10:23:22 -0600

    Arrow Function Exercise

---
+++ arrowFunctionsExercise/app.js
@@ -0,0 +1,27 @@
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+
+const double = (arr) => {
+return arr.map((val)=>val*2)
+}
+
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+ const squareAndFindEvens =(numbers)=>{
+    var squares = numbers.map((num)=> num ** 2
+);
+    var evens = squares.filter((square) => square % 2 === 0
+    );
+    return evens;
+  }
